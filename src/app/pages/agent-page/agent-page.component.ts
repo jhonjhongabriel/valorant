be630c74e032bb1c7d5b5a961994e65cae97fceb
Thiago Augusto <thiago.digitalfront@gmail.com>
2021-03-09 16:25:23 -0300

    details page

--- src/app/pages/agent-page/agent-page.component.ts
+++ src/app/pages/agent-page/agent-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { APIService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-agent-page',
@@ -8,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AgentPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(public http: APIService, private route: ActivatedRoute) { }
+
+  public uuid = this.route.snapshot.params.id;
+  public agent;
+
 
   ngOnInit(): void {
+    this.loadAgent();
+  }
+
+  loadAgent() {
+    return this.http.getAgent(this.uuid).subscribe( res => {
+      this.agent = res
+    })
   }
 
 }
